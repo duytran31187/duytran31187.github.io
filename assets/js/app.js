c177207eb53076b7c74af30bfec745f5e4587a97
Duy Tran <duytran31187@gmail.com>
2023-06-21 20:34:43 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,7 +55,7 @@ const vm = new Vue({
                     }
                 ],
                 summary: [
-                    "I have about more than 4-year experience in <strong>HTML5/CSS3/JS, Bootstrap, JQuery, Angularjs</strong>",
+                    "I have about more than 4-year experience in <strong>HTML5 / CSS3 / JS, Bootstrap, JQuery, Angularjs</strong>",
                     "Less than 1-yer experience with <strong>Reactjs</strong>",
                     "more than 2-year experience in <strong>Aws services: Ec2, s3, cloudwatch, Xray, Gateway, Codecommit, EventBridge, CloudFormation, Lambda, serverless...</strong>",
                     "more than 10-year experience in <strong>PHP</strong> Web Application Development (<strong>Laravel, Yii2, CodeIgniter</strong>)",
